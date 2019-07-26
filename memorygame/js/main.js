d3b8eb338777cb4ce56b5892dfc1e99758ad9aac
Nancy <nancyho629@gmail.com>
2019-07-25 20:48:51 -0400

    created game logic

---
+++ memorygame/js/main.js
@@ -0,0 +1,9 @@
+console.log("Up and running!");
+
+var cardOne = "Queen"
+var cardTwo = "King"
+var cardThree = "Queen"
+var cardFour = "King"
+
+console.log("User Flipped " + cardOne);
+console.log("User Flipped " + cardThree);
